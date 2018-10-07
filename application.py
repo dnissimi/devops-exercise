778f023dc283c060e785dbb20597567befe6d140
Doron Nissimi <dnissimi@gmail.com>
2018-10-07 15:40:50 +0300

    Initial commit

---
+++ application.py
@@ -0,0 +1,30 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+##!bin/python
+
+from flask import Flask, jsonify, abort, make_response, request, url_for
+
+
+################ Initialization params ################
+
+application = Flask(__name__)
+application.config['JSON_AS_ASCII'] = False
+
+#######################################################
+
+
+## 'about' method
+@application.route('/api/v1.0/about', methods=['GET'])
+def about():
+    return make_response(jsonify({'about': 'This is a RESTful test harness for the Tikal interview assignment'}), 200)
+    
+
+## jsonified error request for get_task method
+@application.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=8080, debug=False)
+#    application.run(host='127.0.0.1', port=8080, debug=True)
