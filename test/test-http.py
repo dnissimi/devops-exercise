22868e41236f749b8b5fd5a45da439d00a5eeb9d
Doron Nissimi <dnissimi@gmail.com>
2018-10-07 23:20:36 +0300

    Unit testing

---
+++ test/test-http.py
@@ -0,0 +1,10 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+import requests, sys
+
+#r = requests.get("http://127.0.0.1:8080/api/v1.0/about")
+r = requests.get(sys.argv[1])
+
+if r.status_code != 200:
+    print r.status_code
+    exit(1)
